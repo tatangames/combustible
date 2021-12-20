bb80d7da224248d480bc8581a66afe915a03abdb
jonathan moran <tatangamess@gmail.com>
2021-12-20 16:50:01 -0600

    ajustes password

--- app/Http/Controllers/Admin/Roles/PermisosController.php
+++ app/Http/Controllers/Admin/Roles/PermisosController.php
@@ -78,7 +78,7 @@ class PermisosController extends Controller
             $usuario->activo = $request->toggle;
 
             if($request->password != null){
-                $usuario->password = $request->password;
+                $usuario->password = bcrypt($request->password);
             }
 
             //$usuario->assignRole($request->rol); asigna un rol extra
