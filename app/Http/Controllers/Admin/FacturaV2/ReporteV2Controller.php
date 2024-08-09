19725ef032dbf5ac378752a78dde8223b313c58f
tatangames <54828743+tatangames@users.noreply.github.com>
2024-08-09 11:49:21 -0600

    fix v4

--- app/Http/Controllers/Admin/FacturaV2/ReporteV2Controller.php
+++ app/Http/Controllers/Admin/FacturaV2/ReporteV2Controller.php
@@ -176,7 +176,8 @@ class ReporteV2Controller extends Controller
         }
 
         $tabla .= "<tr>
-                <td colspan='9' style='font-size:11px; text-align: center; font-weight: bold'>TOTAL</td>
+                <td colspan='8' style='font-size:11px; text-align: center; font-weight: bold'>TOTAL</td>
+                 <td style='font-size:11px; text-align: center; font-weight: bold'>$totalGalonesMixtos</td>
                 <td style='font-size:11px; text-align: center; font-weight: bold'>$$totalLinea</td>
             </tr>";
 
