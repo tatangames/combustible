da3865696ae9738bf97b7dd08236289922b18eb6
Jonathan <tatangamess@gmail.com>
2024-11-29 12:11:18 -0600

    reparando centavo de reporte

--- app/Http/Controllers/Admin/FacturaV2/ReporteV2Controller.php
+++ app/Http/Controllers/Admin/FacturaV2/ReporteV2Controller.php
@@ -160,7 +160,7 @@ class ReporteV2Controller extends Controller
         $totalGalonDiesel = number_format((float)$totalGalonDiesel, 2, '.', ',');
         $totalGalonEspecial = number_format((float)$totalGalonEspecial, 2, '.', ',');
 
-        $totalGalonesMixtos = number_format((float)$totalGalonesMixtos, 2, '.', ','); //cambié a 2 decimales, tenía 3.
+        $totalGalonesMixtos = number_format((float)$totalGalonesMixtos, 3, '.', ','); 
 
 
         $infoExtra = Extras::where('id', 1)->first();
